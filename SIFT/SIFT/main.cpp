ea1c1c07975bbf65a4cc075160ad00e0aebc189d
Jaroslav Hrách <me@jaroslavhrach.cz>
2016-11-15 13:59:01 +0100

    vytvoření projektu

---
+++ SIFT/SIFT/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  SIFT
+//
+//  Created by Jaroslav Hrách on 15.11.16.
+//  Copyright © 2016 Jaroslav Hrách. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+}
